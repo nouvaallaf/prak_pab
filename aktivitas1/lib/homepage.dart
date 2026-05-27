d3a522b4942705d65150002358af51f6d625b77b
nouvaallaf <108867069+nouvaallaf@users.noreply.github.com>
2026-05-28 03:04:42 +0700

    Add files via upload

---
+++ aktivitas1/lib/homepage.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page'), centerTitle: true),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+
+            const Text(
+              'PRAKTIKUM PAB 2023',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 40),
+
+            const Text(
+              '1462300132',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 60),
+
+            Image.asset('images/profile.jpg', width: 200, height: 200),
+            const SizedBox(height: 60),
+
+            const Text(
+              'Nouval Abdullah Farouq',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 100),
+
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(100, 50),
+                  backgroundColor: Colors.deepPurple,
+                ),
+                onPressed: () {},
+
+                child: const Text(
+                  'Masuk',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
